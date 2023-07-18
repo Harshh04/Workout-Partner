24fc911588602404a0d4e32685cdd85d5a9b2db8
Harshh04 <harshin416@gmail.com>
2023-07-18 17:34:15 +0530

    Functionality completed

---
+++ frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,41 @@
+import formatDistanceToNowStrict from "date-fns/formatDistanceToNowStrict";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Weight (kg): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Number of reps: </strong>
+        {workout.reps}
+      </p>
+      <p>
+        {formatDistanceToNowStrict(new Date(workout.createdAt), {
+          addSuffix: true,
+        })}
+      </p>
+      <span onClick={handleClick} className="material-symbols-outlined">
+        delete
+      </span>
+    </div>
+  );
+};
+
+export default WorkoutDetails;
